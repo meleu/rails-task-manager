589ad51ace544b4f95df8cd3616382202cde83b9
Augusto Lopes <meleu.dev@gmail.com>
2023-05-23 13:52:26 -0300

    rails task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,41 @@
+class TasksController < ApplicationController
+  before_action :set_task, except: %i[index new create]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to @task
+  end
+
+  def edit; end
+
+  def update
+    @task.update(task_params)
+    redirect_to @task
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,23 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'tasks#index'
+
+  # # Read all
+  # get '/tasks', to: 'tasks#index'
+
+  # # Create a new task
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  # post '/tasks', to: 'tasks#create'
+
+  # # Read one
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+  # # Update a task
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch '/tasks/:id', to: 'tasks#update'
+
+  # # delete a task
+  # delete '/tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
